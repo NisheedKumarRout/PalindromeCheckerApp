ee933497b9eb095ed1aade2697cf747a7add8c56
NisheedKumarRout <nisheedkumarrout@gmail.com>
2026-02-27 15:02:56 +0530

    UC2:Hardcoded palindrome

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,16 +1,12 @@
 public class palindromecheckerapp {
     public static void main(String[] args) {
         String original = "madam";
-        String reversed = "";
+        String reversed = new StringBuilder(original).reverse().toString();
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed += original.charAt(i);
-        }
-
-        if (original.equals(reversed)) {
-            System.out.println(original + " is a palindrome.");
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("The string \"" + original + "\" is a palindrome.");
         } else {
-            System.out.println(original + " is not a palindrome.");
+            System.out.println("The string \"" + original + "\" is not a palindrome.");
         }
     }
 }
