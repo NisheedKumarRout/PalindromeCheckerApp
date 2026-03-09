17cfa08a99810525ffda4799dd13f8b21159126f
NisheedKumarRout <nisheedkumarrout@gmail.com>
2026-03-09 14:43:10 +0530

    UC11: Object oriented palindrome service

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,19 @@
+public class PalindromeChecker {
+
+    // Method to check palindrome
+    public boolean checkPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,32 +1,14 @@
 public class PalindromeCheckerApp {
 
-    // UC10: Case-Insensitive & Space-Ignored Palindrome
-    public static boolean isPalindromeIgnoreCaseSpace(String str) {
-
-        // Normalize string: remove spaces and convert to lowercase
-        str = str.replaceAll("\\s+", "").toLowerCase();
-
-        int start = 0;
-        int end = str.length() - 1;
-
-        // Check palindrome
-        while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-
-        return true;
-    }
-
     public static void main(String[] args) {
 
-        String input = "Madam In Eden Im Adam";
+        String input = "madam";
 
-        if (isPalindromeIgnoreCaseSpace(input)) {
-            System.out.println("Palindrome (ignoring case and spaces)");
+        // Create object of PalindromeChecker
+        PalindromeChecker checker = new PalindromeChecker();
+
+        if (checker.checkPalindrome(input)) {
+            System.out.println("Palindrome");
         } else {
             System.out.println("Not a Palindrome");
         }
